0c4575b0c6104d100effdf7f3e224a276d8e57b6
Tane4ka170 <tane4ka170@gmail.com>
2023-10-26 00:21:55 +0300

    deleted prop-types from api and uptaded styles for modal window

--- src/Helpers/api.js
+++ src/Helpers/api.js
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import PropTypes from 'prop-types';
 
 const API_KEY = '39757407-13e7f5de39700ed4a356d5fcb';
 
@@ -7,11 +6,6 @@ const fetchImages = (query, page) => {
   return axios.get(
     `https://pixabay.com/api/?q=${query}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
   );
-};
-
-fetchImages.propTypes = {
-  query: PropTypes.string.isRequired,
-  page: PropTypes.number.isRequired,
 };
 
 const api = { fetchImages };
